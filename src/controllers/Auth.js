679fc85143b329220f6196547669749901a17ca1
IvanBlacky <tcherivan@gmail.com>
2019-01-24 16:59:07 +0300

    #2 Feat: implement authorization logic

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,10 +1,50 @@
 const core = require('gls-core-service');
 const Basic = core.controllers.Basic;
+const random = require('randomstring');
+const golos = require('golos-js');
 
 class Auth extends Basic {
-    async authorize({ user, sign, secret, ...data }) {}
+    async authorize({ user, sign, secret, ...data }) {
+        const signObject = this._makeUserFakeTransactionObject(user, sign, secret);
 
-    async generateSecret() {}
+        try {
+            await golos.api.verifyAuthorityAsync(signObject);
+        } catch (error) {
+            throw { code: 1103, message: 'Blockchain verification failed - access denied' };
+        }
+
+        return {
+            ...data,
+            sign,
+            user,
+            roles: [],
+        };
+    }
+
+    _makeUserFakeTransactionObject(user, sign, secret) {
+        return {
+            ref_block_num: 3367,
+            ref_block_prefix: 879276768,
+            expiration: '2018-07-06T14:52:24',
+            operations: [
+                [
+                    'vote',
+                    {
+                        voter: user,
+                        author: 'test',
+                        permlink: secret,
+                        weight: 1,
+                    },
+                ],
+            ],
+            extensions: [],
+            signatures: [sign],
+        };
+    }
+
+    async generateSecret() {
+        return random.generate();
+    }
 }
 
 module.exports = Auth;
