88676825ef9f07ef6651b4397a7cfce5855ffcd1
IvanBlacky <tcherivan@gmail.com>
2019-02-01 12:49:55 +0300

    #5 Refactor: remove unused envs

--- src/data/env.js
+++ src/data/env.js
@@ -3,6 +3,4 @@ const env = process.env;
 module.exports = {
     GLS_CONNECTOR_HOST: env.GLS_CONNECTOR_HOST || '127.0.0.0',
     GLS_CONNECTOR_PORT: env.GLS_CONNECTOR_PORT || 3000,
-    GLS_METRICS_HOST: env.GLS_METRICS_HOST || '127.0.0.0',
-    GLS_METRICS_PORT: env.GLS_METRICS_PORT || 8125,
 };
